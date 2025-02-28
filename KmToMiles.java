3602ee5ed368decfef1df5cb9aa5feb80e18fcad
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-03-01 00:02:05 +0530

    [Sanya] Add: Convert Kilometer distance into miles

---
+++ KmToMiles.java
@@ -0,0 +1,14 @@
+class KmToFiles{
+	public static void main(String[] args){
+		// Given distance in kilometers
+        	double kilometers = 10.8;
+
+        	// Calculating distance in miles
+        	double miles = kilometers *1.6;
+
+        	// Displaying the result
+        	System.out.println("The distance " + kilometers + " km in miles is " + miles);
+
+	}
+}
+
